24de56cf2aa0ff0bd8d4cf4f26b5161638e0c1c5
Harsh Lalwani <harshlalwani660@gmail.com>
2022-12-12 16:46:49 +0530

    Encapsulation Implemented

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj=new RWOnly();
+/*     obj.name="Harsh";
+     System.out.println(obj.name);
+      Can't Access member variable of class RWOnly as it is Private in nature so it is not having scope till this class...
+      */
+
+      obj.setName("Harsh");
+      System.out.println(obj.getName());
+  }
 }
